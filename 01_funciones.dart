80ab119a875df5fc94e34783b56b250bfd86b7d4
Jhojan <202100351h@utea.edu.pe>
2024-06-12 07:37:42 -0500

    primer conmit

---
+++ 01_funciones.dart
@@ -0,0 +1,22 @@
+void main (List <String> args){
+  print ("hola mundo");
+  //funciones sin retorno y con retorno
+  int n1=5;
+  int n2=6;
+  String Suma;
+  print("Funciones!!!");
+  String name = "Men Lii";
+  MostrarMensaje(name);
+  print(Suma(n1, n2));
+
+}
+
+void MostrarMensaje(String x){
+  print("-----------------");
+  print("Mensaje de una funcion: $x");
+  print("-----------------");
+}
+
+Summa(int a, int b){
+  return a + b;
+}
