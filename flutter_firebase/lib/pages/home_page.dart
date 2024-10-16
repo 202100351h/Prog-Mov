836545a29c7c841bbcf1712c9483779d1ca8b8a0
Jhojan <202100351h@utea.edu.pe>
2024-10-16 12:59:17 -0500

    aea

---
+++ flutter_firebase/lib/pages/home_page.dart
@@ -0,0 +1,11 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+//import 'package:flutter_codigo_firetask/models/task_model.dart';
+//import 'package:flutter_codigo_firetask/ui/general/colors.dart';
+//import 'package:flutter_codigo_firetask/ui/widgets/general_widgets.dart';
+//import 'package:flutter_codigo_firetask/ui/widgets/item_task_widget.dart';
+//import 'package:flutter_codigo_firetask/ui/widgets/task_form_widget.dart';
+//import 'package:flutter_codigo_firetask/ui/widgets/texfield_normal_widget.dart';
+//import 'package:flutter_codigo_firetask/utils/task_search_delegate.dart';
+
