41554f8852b1d11464bc096997e919b83ec5488e
Jhojan <202100351h@utea.edu.pe>
2024-10-17 00:53:42 -0500

    prueba

--- flutter_firebase/lib/main.dart
+++ flutter_firebase/lib/main.dart
@@ -2,13 +2,12 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_firebase/firebase_options.dart';
 import 'package:flutter_firebase/pages/home_page.dart';
-//import 'package:flutter_firebase/pages/login_page.dart';
+import 'package:flutter_firebase/pages/login_page.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  await Firebase.initializeApp(); // Inicializa Firebase
   runApp(MyApp());
 }
 
@@ -16,11 +15,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Firebase Demo',
+      title: 'Firetask',
       theme: ThemeData(
+        textTheme: GoogleFonts.manropeTextTheme(),
         primarySwatch: Colors.blue,
       ),
-      home: HomePage(),
+      home: LoginPage(), // Redirige al HomePage
     );
   }
 }
